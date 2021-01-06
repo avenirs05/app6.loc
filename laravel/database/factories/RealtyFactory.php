61b2b205292dc2770961c619b9ac190c88d5efba
avenirs05 <developer24@protonmail.com>
2021-01-06 14:52:55 +0300

    last

--- laravel/database/factories/RealtyFactory.php
+++ laravel/database/factories/RealtyFactory.php
@@ -27,7 +27,7 @@ $factory->define(Realty::class, function (Faker $faker) {
         'internet_payment_en'  => $faker->randomElement(['free', '10€', '15€']),
         'parking_payment_ru'   => $faker->randomElement(['бесплатный', '5€', '7€']),
         'parking_payment_en'   => $faker->randomElement(['free', '5€', '7€']), 
-        'description_ru'       => $faker->text(1000) . '\n' . $faker->text(1000),
+        'description_ru'       => $faker->text(1000),
         'description_en'       => $faker->text(2000),
         'map_html'             => '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2951.3632492036777!2d18.867321315101165!3d42.292113947382965!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDLCsDE3JzMxLjYiTiAxOMKwNTInMTAuMiJF!5e0!3m2!1sru!2sru!4v1507902217396" width="100%" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>',
         'square'               => $faker->numberBetween(20, 999),
