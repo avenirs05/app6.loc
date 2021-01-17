2a3cf56c1ad77e122814ba87168a36d29990f861
avenirs05 <developer24@protonmail.com>
2021-01-17 14:06:52 +0300

    last

--- laravel/resources/js/actions/getFileListAction.js
+++ laravel/resources/js/actions/getFileListAction.js
@@ -3,8 +3,8 @@ import { GET_FILE_LIST } from './consts';
 export function getFileListAction(payload, imgType) {    
   return { 
     type: GET_FILE_LIST, 
-    imgType,
-    payload
+    payload,
+    imgType
   }   
 }
 
