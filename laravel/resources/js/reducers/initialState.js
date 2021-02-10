e5e00d1896ea8cfc8cb3dd07b0a238987d10a06a
avenirs05 <developer24@protonmail.com>
2021-02-10 23:01:51 +0300

    last

--- laravel/resources/js/reducers/initialState.js
+++ laravel/resources/js/reducers/initialState.js
@@ -6,14 +6,6 @@ export const feedbacks = {
   perPage: 50 
 }
 
-export const images = {
-  items: [],
-  totalPages: 0,
-  totalItems: 0,
-  currentPage: 1,
-  perPage: 100 
-}
-
 export const realties = {
   items: [],
   totalPages: 0,
